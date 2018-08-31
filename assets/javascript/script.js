bd7449db1885a0f6845b214b537c3510b39b5e1a
Brady <brady.withers@outlook.com>
2018-08-31 10:52:59 -0600

    countdown clock working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,7 +11,28 @@ $("#basicSound").on("click", function () {
     basicSound.play();
 });
 
-
+//create variable for audio file
+var timerSound = new Audio('assets/sounds/timerSound.wav');
+//function for simple timer with start button
+function startTimer(){
+  var counter = 10;
+  setInterval(function() {
+    counter--;
+    if (counter >= 0) {
+      span = document.getElementById("count");
+      span.innerHTML = counter;
+		}
+		//when counter === 0 play sound
+    if (counter === 0) {
+        timerSound.play();
+        clearInterval(counter);
+    }
+  }, 1000);
+}
+//function for on.click to start timer
+$("#startClock").click(function(){
+    startTimer();
+ });
 
 
 
